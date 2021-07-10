729f991a7c55542e9b518cdec15998809b80e2f3
akshatchaturvedi7 <akshatchaturvedi007@gmail.com>
2021-07-10 20:18:30 +0530

    safe

--- twclone/src/Widgets.js
+++ twclone/src/Widgets.js
@@ -1,10 +1,25 @@
 import React from "react";
 import "./Widgets.css";
+import SearchIcon from '@material-ui/icons/Search';
+import {
+    TwitterTimelineEmbed,
+    TwitterShareButton,
+    TwitterTweetEmbed,
+} from "react-twitter-embed";
+
 
 function Widgets(){
     return(
         <div className="widgets">
-            <h2>Widgets</h2>
+            <div className="widgets__input">
+                <SearchIcon className="widgets__searchIcon" />
+                <input placeholder="Search Twitter" type="text" />
+            </div>
+            <div className="widgets__widgetContainer">
+                <h2>What's happening</h2>
+                <TwitterTweetEmbed tweetId={"1413736546373718016"} /> 
+                <TwitterTimelineEmbed sourceTye="profile" screenName="vishy64theking" options={{height: 400}} />
+            </div>
         </div>
     );
 }
